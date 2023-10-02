50ec4479e7c1d23e598d5422e2d321afa9abed10
Abdullah AL Hinaey <101839293+AbdullahALX@users.noreply.github.com>
2023-10-02 14:12:49 -0400

    adding slider

--- src/components/imageSwiper/SwiperImg.js
+++ src/components/imageSwiper/SwiperImg.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from 'swiper/modules';
+
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 import '../imageSwiper/swiperImage.css';
 
 import image_0 from '../../assets/image0.jpeg';
@@ -14,14 +25,31 @@ const SwiperImg = () => {
   return (
     <section id="Home" className="homeContent">
       <Swiper
+        style={{
+          '--swiper-pagination-color': '#72655c',
+          '--swiper-pagination-bullet-inactive-color': '#999999',
+          '--swiper-pagination-bullet-inactive-opacity': '1',
+          '--swiper-pagination-bullet-size': '14px',
+          '--swiper-pagination-bullet-horizontal-gap': '6px',
+        }}
+        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
         spaceBetween={50}
         slidesPerView={1}
-        onSlideChange={() => console.log('slide change')}
+        pagination={{ clickable: true }}
+        autoplay={{ delay: 2000, loop: true }}
+        //scrollbar={{ draggable: true }}
         onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
       >
         <SwiperSlide>
           <div className="imgContainer">
-            <img src={image_0} alt="" />
+            <img src={image_3} alt="" />
+            <div className="textCover">
+              <h1 className="text">
+                Your Vision Into A Design <br></br>
+              </h1>
+              <h1 className="textName">by embroideries by ari</h1>
+            </div>
           </div>
         </SwiperSlide>
 
@@ -39,7 +67,7 @@ const SwiperImg = () => {
 
         <SwiperSlide>
           <div className="imgContainer">
-            <img src={image_3} alt="" />
+            <img src={image_0} alt="" />
           </div>
         </SwiperSlide>
       </Swiper>
